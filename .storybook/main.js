417577cd96cd1efd94edc0ee65fee7ff98038d2e
Davide Fiorello <davide@codeflyer.com>
2021-07-26 19:01:37 +0200

    import project

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+        },
+      },
+    },
+  ],
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+import '../src/reset.css'
+import '../src/typography.css'
+import '../src/theme.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+}
